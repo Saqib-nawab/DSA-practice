13ce1f2e1d76e045cb99a906b3e2f5b68dada327
Saqib <saqibnawab823@gmail.com>
2024-03-11 17:35:46 +0500

    pre-order tree travrsial

--- treelinked.c
+++ treelinked.c
@@ -18,38 +18,38 @@ struct node *createNode(int data)
     return n;                                       // Finally returning the created node
 }
 
+void preOrder(struct node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
 int main()
 {
-    /*
-    // Constructing the root node
-    struct node *p;
-    p = (struct node *) malloc(sizeof(struct node));
-    p->data = 2;
-    p->left = NULL;
-    p->right = NULL;
-
-    // Constructing the second node
-    struct node *p1;
-    p1 = (struct node *) malloc(sizeof(struct node));
-    p->data = 1;
-    p1->left = NULL;
-    p1->right = NULL;
-
-    // Constructing the third node
-    struct node *p2;
-    p2 = (struct node *) malloc(sizeof(struct node));
-    p->data = 4;
-    p2->left = NULL;
-    p2->right = NULL;
-    */
 
     // Constructing the root node - Using Function (Recommended)
-    struct node *p = createNode(2);
+    struct node *p = createNode(4);
     struct node *p1 = createNode(1);
-    struct node *p2 = createNode(4);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(5);
+    struct node *p4 = createNode(2);
+    // Finally The tree looks like this:
+    //      4
+    //     / \
+    //    1   6
+    //   / \
+    //  5   2
 
     // Linking the root node with left and right children
     p->left = p1;
     p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    preOrder(p);
     return 0;
 }
