146f326bd3da8abe71410b33b6351dff35d07132
Saqib <saqibnawab823@gmail.com>
2024-03-11 19:00:51 +0500

    iterative searching in BST commented

--- isbst.c
+++ isbst.c
@@ -90,6 +90,26 @@ struct node *search(struct node *root, int key)
     }
 }
 
+// struct node *searchIter(struct node *root, int key)
+// {
+//     while (root != NULL)
+//     {
+//         if (key == root->data)
+//         {
+//             return root;
+//         }
+//         else if (key < root->data)
+//         {
+//             root = root->left;
+//         }
+//         else
+//         {
+//             root = root->right;
+//         }
+//     }
+//     return NULL;
+// }
+
 int main()
 {
 
